e053485d1ecdb5be4d0d4cd6480daf82db0495a9
sh3eys <sharaku3eyes@gmail.com>
2020-09-27 19:11:17 +0900

    try to db-connection

--- docker/app/index.js
+++ docker/app/index.js
@@ -2,8 +2,46 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const mysql = require('mysql');
+const log4js = require('log4js')
+
+log4js.configure({
+  appenders : {
+    system : {type : 'file', filename : '/tmp/system.log'}
+  },
+  categories : {
+    default : {appenders : ['system'], level : 'debug'},
+  }
+});
+const logger = log4js.getLogger('system');
+
+
+const connection = mysql.createConnection({
+  host: 'telepath-db',
+  user: 'root',
+  password: '',
+  database: 'telepath'
+});
+
+connection.connect((err) => {
+  if (err) {
+      logger.debug('error connecting: ' + err.stack)
+      return;
+  }
+  logger.debug('success')
+});
+
+var msg_db;
+connection.query(
+  'SELECT * FROM chat_line',
+  (error, results) => {
+    msg_db = results;
+  }
+);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  io.emit('chat message', msg_db);
 });
 
 io.on('connection', (socket) => {
